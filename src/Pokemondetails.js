c645ae905ad925cd1f6631034e9d179db921d29d
Sam McGrail <sammcgrail@gmail.com>
2023-09-04 23:19:37 -0400

    decimeteres to meters

--- src/Pokemondetails.js
+++ src/Pokemondetails.js
@@ -38,9 +38,10 @@ function PokemonDetails() {
 
   return (
     <div>
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
       <h2>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
-      <p>Height: {pokemon.height}</p>
-      <p>Weight: {pokemon.weight}</p>
+      <p>Height: {pokemon.height / 10} meters</p>
+      <p>Weight: {pokemon.weight / 10} kg</p>
     </div>
   );
 }
