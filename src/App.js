f15e6a0058bc71b96c2175a6773ff3967cb64f21
Sam McGrail <sammcgrail@gmail.com>
2023-09-05 13:26:38 -0400

    add bar chart at bottom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PokemonDetails from "./Pokemondetails";
+import { Chart, BarElement, CategoryScale, LinearScale } from "chart.js";
+import { Bar as BarChartComponent } from "react-chartjs-2";
+
+Chart.register(BarElement, CategoryScale, LinearScale);
 
 export const POKEMON_API_URL = "https://pokeapi.co/api/v2/pokemon";
 
@@ -18,19 +22,13 @@ function App() {
     const fetchPokemons = async () => {
       const response = await fetch(`${POKEMON_API_URL}?limit=151`);
       const body = await response.json();
-
       const pokemonPromises = body.results.map((pokemon) =>
         fetchPokemonDetails(pokemon.url)
       );
-
       const pokemonDetails = await Promise.all(pokemonPromises);
-
-      // Sort Pokemon by height
       const sortedPokemon = pokemonDetails.sort((a, b) => a.height - b.height);
-
       setPokemonList(sortedPokemon);
     };
-
     fetchPokemons();
   }, []);
 
@@ -48,64 +46,82 @@ function App() {
 }
 
 function HomePage({ pokemonList }) {
-  const [sortField, setSortField] = useState("id");
-  const [sortOrder, setSortOrder] = useState("asc");
+  const [sortedPokemonList, setSortedPokemonList] = useState(pokemonList);
+  const [sortingKey, setSortingKey] = useState("id");
+  const [ascending, setAscending] = useState(true);
 
-  const sortedPokemonList = [...pokemonList].sort((a, b) => {
-    if (!sortField) return 0;
+  useEffect(() => {
+    let sortedList = [...pokemonList].sort((a, b) => {
+      if (ascending) {
+        return a[sortingKey] - b[sortingKey];
+      } else {
+        return b[sortingKey] - a[sortingKey];
+      }
+    });
+    setSortedPokemonList(sortedList);
+  }, [pokemonList, sortingKey, ascending]);
 
-    // Special handling for types since it's an array
-    if (sortField === "types") {
-      const aType = a.types[0].type.name;
-      const bType = b.types[0].type.name;
-      return sortOrder === "asc"
-        ? aType.localeCompare(bType)
-        : bType.localeCompare(aType);
-    }
+  const handleSort = (key) => {
+    setAscending((prev) => (key === sortingKey ? !prev : true));
+    setSortingKey(key);
+  };
 
-    if (sortOrder === "asc") {
-      return a[sortField] > b[sortField] ? 1 : -1;
-    } else {
-      return a[sortField] < b[sortField] ? 1 : -1;
-    }
-  });
-
-  const handleSort = (field) => {
-    if (sortField === field) {
-      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
-    } else {
-      setSortField(field);
-      setSortOrder("asc");
-    }
+  const chartData = {
+    labels: sortedPokemonList.map((pokemon) => pokemon.name),
+    datasets: [
+      {
+        label: "Height (m)",
+        data: sortedPokemonList.map((pokemon) => pokemon.height / 10),
+        backgroundColor: "rgba(75, 192, 192, 0.6)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+      {
+        label: "Weight (kg)",
+        data: sortedPokemonList.map((pokemon) => pokemon.weight / 10),
+        backgroundColor: "rgba(153, 102, 255, 0.6)",
+        borderColor: "rgba(153, 102, 255, 1)",
+        borderWidth: 1,
+      },
+    ],
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th onClick={() => handleSort("id")}>#</th>
-          <th onClick={() => handleSort("name")}>Name</th>
-          <th onClick={() => handleSort("height")}>Height (m)</th>
-          <th onClick={() => handleSort("weight")}>Weight (kg)</th>
-          <th onClick={() => handleSort("types")}>Type</th>
-        </tr>
-      </thead>
-      <tbody>
-        {sortedPokemonList.map((pokemon) => (
-          <tr key={pokemon.id}>
-            <td>{pokemon.id}</td>
-            <td>
-              <Link to={`/pokemon/${pokemon.id}`} style={{ color: "white" }}>
-                {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
-              </Link>
-            </td>
-            <td>{pokemon.height / 10}</td>
-            <td>{pokemon.weight / 10}</td>
-            <td>{pokemon.types.map((type) => type.type.name).join(", ")}</td>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th onClick={() => handleSort("id")}>ID</th>
+            <th onClick={() => handleSort("name")}>Name</th>
+            <th onClick={() => handleSort("height")}>Height</th>
+            <th onClick={() => handleSort("weight")}>Weight</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {sortedPokemonList.map((pokemon) => (
+            <tr key={pokemon.id}>
+              <td>{pokemon.id}</td>
+              <td>{pokemon.name}</td>
+              <td>{pokemon.height / 10} m</td>
+              <td>{pokemon.weight / 10} kg</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <BarChartComponent
+        data={chartData}
+        options={{
+          indexAxis: "y",
+          scales: {
+            x: {
+              type: "linear",
+              position: "bottom",
+              beginAtZero: true,
+            },
+          },
+        }}
+      />
+    </div>
   );
 }
 
