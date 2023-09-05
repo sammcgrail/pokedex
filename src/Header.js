6235f1302e3ae196046938d2c97c364f0a63ead3
Sam McGrail <sammcgrail@gmail.com>
2023-09-04 23:19:08 -0400

    ball on header

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Header() {
-  return <h1>Pokémon Sorted by Height</h1>;
+  return (
+    <div className="header">
+      <img src="/ball.png" alt="Pokéball" className="pokeball-icon" />
+      <h1>Pokédex</h1>
+    </div>
+  );
 }
 
 export default Header;
